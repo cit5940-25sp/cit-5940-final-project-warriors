65b89d537f0fd917969f3b0e0b841795ce80739c
bkee2 <bkee@seas.upenn.edu>
2025-04-07 20:33:27 -0400

    Created classes based of class diagram

---
+++ src/IObservable.java
@@ -0,0 +1,2 @@
+public interface IObservable {
+}
---
+++ src/Movie.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Movie {
+    private String title;
+    private int releaseDate;
+    private List<String> genres;
+    private List<String> directors;
+    private List<String> actors;
+    private List<String> writers;
+    private List<String> cinematographers;
+    private List<String> composers;
+
+
+
+
+}
