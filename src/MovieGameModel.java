649a7b053a9f16d779715f75d99c21a8255f67e7
bkee2 <bkee@seas.upenn.edu>
2025-04-14 16:03:34 -0400

    Updated to include interfaces

--- src/MovieGameModel.java
+++ src/MovieGameModel.java
@@ -1,2 +1,33 @@
+import java.util.*;
+
 public class MovieGameModel implements IObservable {
+
+    private GameState gameState;
+    private List<IObserver> observers;
+
+    public MovieGameModel() {
+        this.gameState = new GameState();
+        this.observers = new ArrayList<>();
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+
+    @Override
+    public void addObserver(IObserver o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(IObserver o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+
 }
--- src/MovieGameView.java
+++ src/MovieGameView.java
@@ -1,4 +1,14 @@
+import java.util.Observable;
+
 public class MovieGameView implements IObserver {
 
+
+
+
+
+    @Override
+    public void update(Observable o, Object arg) {
+        //Movie movie = (Movie) arg;
+    }
 
 }
