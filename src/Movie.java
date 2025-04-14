9a69982688828f6270e7c68ca7be14c06fe0c998
bkee2 <bkee@seas.upenn.edu>
2025-04-14 13:32:12 -0400

    Added constructor and methods to Movie class

--- src/Movie.java
+++ src/Movie.java
@@ -10,7 +10,85 @@ public class Movie {
     private List<String> cinematographers;
     private List<String> composers;
 
+    public Movie(String title, int releaseDate,
+                 List<String> genres,
+                 List<String> directors,
+                 List<String> actors,
+                 List<String> writers,
+                 List<String> cinematographers,
+                 List<String> composers) {
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.genres = genres;
+        this.directors = directors;
+        this.actors = actors;
+        this.writers = writers;
+        this.cinematographers = cinematographers;
+        this.composers = composers;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public List<String> getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(List<String> directors) {
+        this.directors = directors;
+    }
+
+    public List<String> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<String> actors) {
+        this.actors = actors;
+    }
+
+    public List<String> getWriters() {
+        return writers;
+    }
+
+    public void setWriters(List<String> writers) {
+        this.writers = writers;
+    }
+
+    public List<String> getCinematographers() {
+        return cinematographers;
+    }
+
+    public void setCinematographers(List<String> cinematographers) {
+        this.cinematographers = cinematographers;
+    }
+
+    public List<String> getComposers() {
+        return composers;
+    }
+
+    public void setComposers(List<String> composers) {
+        this.composers = composers;
+    }
 
 }
