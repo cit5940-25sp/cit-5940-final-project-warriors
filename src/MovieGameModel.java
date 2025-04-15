86fb5c81ad5735645ab53e4aa1fca3d9cb939c0b
bkee2 <bkee@seas.upenn.edu>
2025-04-15 12:59:57 -0400

    Added update and notifyObserver methods

--- src/MovieGameModel.java
+++ src/MovieGameModel.java
@@ -6,46 +6,72 @@ public class MovieGameModel implements IObservable {
     private List<IObserver> observers;
     private boolean hasChanged = false;
 
+    /**
+     * Initialize a new MovieGameModel
+     */
     public MovieGameModel() {
         this.gameState = new GameState();
         this.observers = new ArrayList<>();
     }
 
+    /**
+     * @return the current gameState
+     */
     public GameState getGameState() {
         return gameState;
     }
 
-
+    /**
+     * Adds a new observer
+     */
     @Override
     public void addObserver(IObserver o) {
         observers.add(o);
     }
 
+    /**
+     * Removes an observer
+     */
     @Override
     public void removeObserver(IObserver o) {
         observers.remove(o);
     }
 
+    /**
+     * Clears all observers
+     */
     @Override
     public void removeAllObservers() {
         observers.clear();
     }
 
+    /**
+     * Update observer status
+     */
     @Override
     public void setChanged() {
         hasChanged = true;
     }
 
+    /**
+     * Reset an observer
+     */
     @Override
     public void clearChanged() {
         hasChanged = false;
     }
 
+    /**
+     * @return if an observer has been updated
+     */
     @Override
     public boolean hasChanged() {
         return hasChanged;
     }
 
+    /**
+     * Updates observers if there has been a change
+     */
     @Override
     public void notifyObservers(String event) {
         if (hasChanged) {
--- src/MovieGameView.java
+++ src/MovieGameView.java
@@ -1,14 +1,39 @@
 import java.util.Observable;
 
 public class MovieGameView implements IObserver {
+    private MovieGameModel model;
 
+    /**
+     * Initializes a new MovieGameView
+     */
+    public MovieGameView(MovieGameModel model) {
+        this.model = model;
+        model.addObserver(this);
+    }
 
-
-
-
+    /**
+     * Update the GameView based on the event that
+     * has occurred
+     */
     @Override
     public void update(String event) {
-        //Movie movie = (Movie) arg;
+        switch (event) {
+            case "GAME_START":
+                // get new display of game
+                break;
+            case "VALID_MOVE":
+                // update to reflect move made
+                break;
+            case "INVALID_MOVE":
+                // show error message
+                break;
+            case "GAME_END":
+                // show display of end screen
+                break;
+            default:
+                // update the entire view
+                break;
+        }
     }
 
 }
