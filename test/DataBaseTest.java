3d7b37428cacbb568f843a204704745e6f113c6c
amlxn-ahihi <anhle99.official@gmail.com>
2025-04-17 19:24:18 -0400

    Update database

---
+++ test/DataBaseTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataBaseTest {
+    @Test
+    public void testDatabase() {
+        DataBase db = new DataBase();
+        db.loadFromCSV("cleaned_imdb.csv");
+        db.printDatabase();
+    }
+}
