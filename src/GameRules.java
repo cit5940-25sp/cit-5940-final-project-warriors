71be08499f5245507c659ab5d86aee0b8a47315d
paulawang <paulatw@seas.upenn.edu>
2025-04-17 23:36:12 -0400

    Updated GameRules

--- src/GameRules.java
+++ src/GameRules.java
@@ -1,5 +1,5 @@
 public class GameRules {
-    private int rounds;
-    private int scoreToWin;
-    private int maxUsage;
+    private final int MAX_ROUNDS = 30; // arbitrary, ignore
+    private final int WIN_SCORE = 100; // arbitrary, ignore
+    private final int COUNTDOWN_SECS = 15;
 }
