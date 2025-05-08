d41af8ff088b977666f8d3ef176bbefa60892448
amlxn-ahihi <anhle99.official@gmail.com>
2025-05-08 11:04:14 -0400

    clean up database

--- src/Database.java
+++ src/Database.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Database {
@@ -192,10 +190,6 @@ public class Database {
      * @return Movie
      */
     public Movie getRandomMovie() {
-//        Set<String> keySet = movieMap.keySet();
-//        int randomIndex = new Random().nextInt(keySet.size());
-//        String randomKey = (String) keySet.toArray()[randomIndex];
-//        return movieMap.get(randomKey);
         return movieMap.get("Titanic (1997)");
     }
 
@@ -221,114 +215,4 @@ public class Database {
         System.err.println("Movie not found in database.");
         return null;
     }
-
-
-//    /**
-//     * @return the type of connection between two movies. Return NULL
-//     * if no connection is possible.
-//     */
-//    public String getConnectionType(Movie movie1, Movie movie2) {
-//
-//        for (String actor : movie1.getActors()) {
-//            if (movie2.getActors().contains(actor)) {
-//                return "actor";
-//            }
-//        }
-//
-//        for (String director : movie1.getDirectors()) {
-//            if (movie2.getDirectors().contains(director)) {
-//                return "director";
-//            }
-//        }
-//
-//        for (String cinematographer : movie1.getCinematographers()) {
-//            if (movie2.getCinematographers().contains(cinematographer)) {
-//                return "cinematographer";
-//            }
-//        }
-//
-//        for (String composer : movie1.getComposers()) {
-//            if (movie2.getComposers().contains(composer)) {
-//                return "composer";
-//            }
-//        }
-//
-//        for (String writer : movie1.getWriters()) {
-//            if (movie2.getWriters().contains(writer)) {
-//                return "writer";
-//            }
-//        }
-//
-//        return null;
-//    }
-//
-//    /**
-//     * @return the set of shared connection values based on the
-//     * type of connection. Return NULL if no valid connection.
-//     */
-//    public String getConnection(Movie movie1, Movie movie2, String connectionType) {
-//        if (connectionType == null) {
-//            return null;
-//        }
-//
-//        Set<String> movie1Set;
-//        Set<String> movie2Set;
-//
-//        switch (connectionType) {
-//            case "actor":
-//                movie1Set = movie1.getActors();
-//                movie2Set = movie2.getActors();
-//                break;
-//            case "director":
-//                movie1Set = movie1.getDirectors();
-//                movie2Set = movie2.getDirectors();
-//                break;
-//            case "cinematographer":
-//                movie1Set = movie1.getCinematographers();
-//                movie2Set = movie2.getCinematographers();
-//                break;
-//            case "composer":
-//                movie1Set = movie1.getComposers();
-//                movie2Set = movie2.getComposers();
-//                break;
-//            case "writer":
-//                movie1Set = movie1.getWriters();
-//                movie2Set = movie2.getWriters();
-//                break;
-//            default:
-//                return null;
-//        }
-//
-//        for (String name : movie1Set) {
-//            if (movie2Set.contains(name)) {
-//                return name;
-//            }
-//        }
-//
-//        return null;
-//    }
-//
-//    /**
-//     * @return TRUE if the connection is valid, FALSE otherwise
-//     */
-//    public boolean checkConnection(Movie currentMovie, Movie nextMovie) {
-//        String connectionType = getConnectionType(currentMovie, nextMovie);
-//
-//        if (connectionType == null) {
-//            return false;
-//        }
-//
-//        String connection = getConnection(currentMovie, nextMovie, connectionType);
-//        if (connection == null) {
-//            return false;
-//        }
-//
-//        // check connection hasn't been used 3 or more times
-//        if (connectionsUsed.getOrDefault(connection, 0) >= 3) {
-//            return false;
-//        }
-//
-//        return true;
-//    }
-
 }
